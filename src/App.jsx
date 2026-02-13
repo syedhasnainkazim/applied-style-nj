73cbc5faeda10a1acb303a5876e4866182a304fc
hasnainkaz1 <syedmahkazim110@gmail.com>
2026-02-13 14:07:16 -0500

    Ensure emailjs installed

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,20 @@ import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import VinylWrap from "./pages/VinylWrap";
+import GetQuote from "./pages/GetQuote"; // ✅ MAKE SURE THIS EXISTS
 
 function App() {
   return (
     <Router>
-      <Routes>
-        {/* Layout wraps all pages */}
-        <Route element={<Layout />}>
+      <Layout>
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
           <Route path="/services/vinyl-wraps" element={<VinylWrap />} />
-        </Route>
-      </Routes>
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/get-quote" element={<GetQuote />} /> {/* ✅ IMPORTANT */}
+        </Routes>
+      </Layout>
     </Router>
   );
 }
